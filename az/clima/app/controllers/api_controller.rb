1168214b5f0ae5c60eb7f17ac6ec381e2920b241
Jose Zapata <programar.az@gmail.com>
2018-08-13 21:10:59 -0500

    Clase13AGO

--- az/clima/app/controllers/api_controller.rb
+++ az/clima/app/controllers/api_controller.rb
@@ -1,7 +1,10 @@
 # Se crea un controlador especifico para la base.
 
     class ApiController < ActionController::Base
+        include Knock::Authenticable
+
         before_action :set_default_format
+        before_action :authenticate_user
         skip_before_action :verify_authenticity_token, raise: false
 
     private
